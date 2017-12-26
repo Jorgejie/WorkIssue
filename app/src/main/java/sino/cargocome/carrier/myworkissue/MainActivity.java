03ce09a58b988bfa4669ad78add55f33fec93286
姬亚杰 <1299891195@qq.com>
2017-12-26 17:21:54 +0800

    新建项目

---
+++ app/src/main/java/sino/cargocome/carrier/myworkissue/MainActivity.java
@@ -0,0 +1,13 @@
+package sino.cargocome.carrier.myworkissue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
